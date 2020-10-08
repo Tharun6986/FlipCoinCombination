192695bffde487c87232550f7ab655d7192bcfde
Tharun6986 <m.tharunsrikanthreddy@gmail.com>
2020-10-08 11:38:53 +0530

    UserCase2 done

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 echo "Welcome to flip coin combination program"
 
 ##UserCase1
@@ -9,4 +9,31 @@ then
 else
 	echo "Tails"
 fi
+
+##UserCase2
+declare -A dictionary;
+dictionary[H]=0;
+dictionary[T]=0;
+count=0;
+
+storeInDictionary(){
+if [ $temp -eq 0 ]
+then
+	dictionary[H]=$(( ${dictionary[H]}+1 ));
+else
+	dictionary[T]=$(( ${dictionary[T]}+1 ));
+fi
+}
+
+while [ $count -le 25 ]
+do
+temp=$((RANDOM%2))
+storeInDictionary $temp;
+((++count));
+done
+
+HeadPercentage=$(( ${dictionary[H]} * 100 / 25 ));
+TailPercentage=$(( 100-$HeadPercentage ));
+echo "Head Percentage is: " $HeadPercentage
+echo "Tail Percentage is: " $TailPercentage
 
