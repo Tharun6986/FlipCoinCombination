55bed8bba0dd483c5e69996ae80ea68db2fcb12d
Tharun6986 <m.tharunsrikanthreddy@gmail.com>
2020-10-08 12:21:01 +0530

    UserCase3 is done

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 echo "Welcome to flip coin combination program"
 
 ##UserCase1
@@ -37,3 +37,40 @@ TailPercentage=$(( 100-$HeadPercentage ));
 echo "Head Percentage is: " $HeadPercentage
 echo "Tail Percentage is: " $TailPercentage
 
+##UserCase3
+declare -A dictionary;
+dictionary[HH]=0;
+dictionary[TT]=0;
+dictionary[HT]=0;
+dictionary[TH]=0;
+count=0;
+
+storeInDictionary(){
+if [ $temp -eq 0 ]
+then
+	dictionary[HH]=$(( ${dictionary[HH]}+1 ));
+elif [ $temp -eq 1 ]
+then
+	dictionary[TT]=$(( ${dictionary[TT]}+1 ));
+else
+	dictionary[HT]=$(( ${dictionary[HT]}+1 ));
+	dictionary[TH]=$(( ${dictionary[TH]}+1 ));
+fi
+}
+
+while [ $count -le 25 ]
+do
+temp=$((RANDOM%3));
+storeInDictionary $temp;
+((++count))
+done
+
+HeadHeadPercentage=$(( ${dictionary[HH]} * 100 / 25 ));
+TailTailPercentage=$(( ${dictionary[TT]} * 100 / 25 ));
+HeadTailPercentage=$(( ${dictionary[HT]} * 100 / 25 ));
+TailHeadPercentage=$(( ${dictionary[TH]} * 100 / 25 ));
+
+echo "HH percentage is: " $HeadHeadPercentage
+echo "TT percentage is: " $TailTailPercentage
+echo "HT percentage is: " $HeadTailPercentage
+echo "TH percentage is: " $TailHeadPercentage
