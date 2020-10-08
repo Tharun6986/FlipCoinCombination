49ebbf6b3c89383934451cf5459953c805d3960c
Tharun6986 <m.tharunsrikanthreddy@gmail.com>
2020-10-08 11:19:42 +0530

    UserCase1 done

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 echo "Welcome to flip coin combination program"
 
+##UserCase1
+randomCheck=$((RANDOM%2));
+if [ $randomCheck -eq 1 ]
+then
+	echo "Heads"
+else
+	echo "Tails"
+fi
+
